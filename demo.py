67e6052fa191717810451a234ebcbedc667a52e8
Livvy <zhengyan928@gmail.com>
2013-10-12 09:17:31 +0800

    demo

---
+++ demo.py
@@ -0,0 +1,5 @@
+Python 2.7.5 (default, May 15 2013, 22:44:16) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print 'hello world'
+hello world
+>>> 
